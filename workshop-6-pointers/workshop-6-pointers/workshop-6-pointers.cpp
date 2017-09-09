a7c1d50eb142428b761be9f5e925d7098d2b970a
TolgaV <tolga.varol@outlook.com>
2017-09-09 18:49:53 +0200

    Workshop 6 added

---
+++ workshop-6-pointers/workshop-6-pointers/workshop-6-pointers.cpp
@@ -0,0 +1,42 @@
+// workshop-6-pointers.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+const int MAX = 20;
+
+void bigChar2end(char*);
+
+int main()
+{
+	char str[MAX];
+	cout << "Enter a string: ";
+	cin >> str;
+	bigChar2end(str);
+	system("pause");
+    return 0;
+}
+
+void bigChar2end(char* str)
+{
+	char *p = &str[0];
+	char *q = &str[1];
+
+	while (*q != '\0')
+	{
+		if (*p > *q)
+		{
+			char temp = *p;
+			*p = *q;
+			*q = temp;
+		}
+
+		++p;
+		++q;
+	}
+	cout << "Final string is: " << str << endl;
+
+	cout << endl;
+}
+
